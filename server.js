6bbf03b37ad4fd3d49f6b30cfa7becb6850a7238
prashantmore45 <prashantmorepm05@gmail.com>
2025-11-15 01:02:14 +0530

    Updated the srever.js

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-
-app.use(express.static(path.join(__dirname, "../public")));
-
-
-app.use("/api/contact", require("./routes/contact"));
+// Routes
 app.use("/api/projects", require("./routes/projects"));
+app.use("/api/contact", require("./routes/contact"));
 app.use("/api/visitors", require("./routes/visitor"));
 app.use("/api/resume", require("./routes/resume"));
 
-
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-
-
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
