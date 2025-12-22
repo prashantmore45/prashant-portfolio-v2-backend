eb6a32966f1d5811f08bfa47d9829cc3cf489020
prashantmore45 <prashantmorepm05@gmail.com>
2025-12-23 00:04:40 +0530

    feat(database): add MongoDB connection setup and dotenv configuration

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+dotenv.config();         
+connectDB();   
+       
 const app = express();
 
 app.use(cors());
